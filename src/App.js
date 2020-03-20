7c6352f986441409b8bd8c74e05432b546e31632
carpethugs <danmezh@gmail.com>
2020-03-19 22:08:48 -0400

    component structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import StartMenu from 'components'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <StartMenu></StartMenu>
     </div>
   );
 }
---
+++ src/components/StartMenu.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function StartMenu(){
+
+}
+
+export default StartMenu;
