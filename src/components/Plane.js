a8c3ee6e97916a72d2d23316922f8204ad8794f6
unknown <calvin@stuntfactory.com>
2020-03-24 23:26:56 -0400

    added airplane

--- src/components/Plane.js
+++ src/components/Plane.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import Image from 'react-bootstrap/Image'
+import React, { Component } from "react";
+import Image from "react-bootstrap/Image";
+import { Popover, OverlayTrigger } from "react-bootstrap";
 
-export default function Plane() {
+export default class Track extends Component {
+  constructor(props) {
+    super(props);
+    const openPlane = (
+      <Popover>
+        <Popover.Title as="h3">title</Popover.Title>
+        <Popover.Content>
+          This is the content {this.props.message}
+        </Popover.Content>
+      </Popover>
+    );
+    this.state = {
+      displayMessage: openPlane
+    };
+  }
+
+  render() {
     return (
-        <div >
-            <Image src={require('../images/paper-plane-3.png')} ></Image>
+      <div>
+        <OverlayTrigger
+          trigger="click"
+          placement="left"
+          overlay={this.state.displayMessage}
+        >
+          <Image src={require("../images/paper-plane-3.png")}></Image>
+        </OverlayTrigger>
+
+        <div>
+          <h1>My info is {this.props.message}</h1>
         </div>
-    )
+      </div>
+    );
+  }
 }
