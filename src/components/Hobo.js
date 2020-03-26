cf2d6d489f4ab8cd8328c8963e07d4b2a240b685
unknown <calvin@stuntfactory.com>
2020-03-26 14:58:52 -0400

    Plane displays info on mouse over

--- src/components/Hobo.js
+++ src/components/Hobo.js
@@ -1,68 +1,77 @@
-import React, {Component} from 'react'
-import Image from 'react-bootstrap/Image'
-import Plane from './Plane'
-import Clock from './Clock'
-
+import React, { Component } from "react";
+import Image from "react-bootstrap/Image";
+import Plane from "./Plane";
+import Clock from "./Clock";
 
 export default class Hobo extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            tracks : this.props.numTracks,
-            nextTrain : this.props.nextTrain,
-            nextTrainTime : this.props.trainTime,
-            message: '',
-        }
-        this.whereIsNextTrain = this.whereIsNextTrain.bind(this)
-    }
-    componentWillReceiveProps(nextProps) {
-        this.setState({ 
-            nextTrain : nextProps.nextTrain,
-            nextTrainTime: nextProps.trainTime })
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracks: this.props.numTracks,
+      nextTrain: this.props.nextTrain,
+      nextTrainTime: this.props.trainTime,
+      message: "Test message(you can delete this Daniel)"
+    };
+    this.whereIsNextTrain = this.whereIsNextTrain.bind(this);
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      nextTrain: nextProps.nextTrain,
+      nextTrainTime: nextProps.trainTime
+    });
+  }
+
+  //Updates self state on either accurate or non-accurate train info
+  whereIsNextTrain() {
+    var mess;
+    if (this.randomNum(10) < 9) {
+      mess =
+        "Next Train is going to be on track " +
+        this.state.nextTrain +
+        " at " +
+        this.state.nextTrainTime;
+    } else {
+      mess = this.generateBadInfo() + " FALSE";
     }
 
-    //Updates self state on either accurate or non-accurate train info
-    whereIsNextTrain(){
-        var mess
-        if(this.randomNum(10)<9){
-            mess="Next Train is going to be on track " + this.state.nextTrain + " at " +this.state.nextTrainTime
-        }
-        else{
-            mess=this.generateBadInfo() + " FALSE"
-        }
+    this.setState({ message: mess });
+    alert(mess);
+  }
 
-        this.setState({message : mess })
-        alert(mess)
-    }
-    
-    randomNum(range){
-        return Math.floor(Math.random()*range)
-    }
-    //Generates lie about train location  
-    generateBadInfo(){
-        let currentTime = new Date()
-        // if(this.randomNum(10)<9){
-        //     falseTrack=this.state.nextTrain
-        //     falseTime=
-        // }
-        // else{
-            var falseTrack=this.randomNum(this.state.tracks)+1
-            var falseTime= new Date(this.randomNum(20000)+currentTime.getTime())
-        // }
-        return "Next Train is going to be on track " + falseTrack + " at " + falseTime.toLocaleTimeString()
-    }
+  randomNum(range) {
+    return Math.floor(Math.random() * range);
+  }
+  //Generates lie about train location
+  generateBadInfo() {
+    let currentTime = new Date();
+    // if(this.randomNum(10)<9){
+    //     falseTrack=this.state.nextTrain
+    //     falseTime=
+    // }
+    // else{
+    var falseTrack = this.randomNum(this.state.tracks) + 1;
+    var falseTime = new Date(this.randomNum(20000) + currentTime.getTime());
+    // }
+    return (
+      "Next Train is going to be on track " +
+      falseTrack +
+      " at " +
+      falseTime.toLocaleTimeString()
+    );
+  }
 
-    render(){
-        return (
-            <div >
-                <div >
-                    <Image src={require('../images/hobo2.png')}
-                    onClick={()=>this.whereIsNextTrain()}
-                    // className='position-absolute'
-                    />
-                    <Plane id='paper_plane' info={this.state.message}/>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <Image
+            src={require("../images/hobo2.png")}
+            onClick={() => this.whereIsNextTrain()}
+            // className='position-absolute'
+          />
+          <Plane id="paper_plane" message={this.state.message} />
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Plane.js
+++ src/components/Plane.js
@@ -1,37 +1,42 @@
 import React, { Component } from "react";
 import Image from "react-bootstrap/Image";
-import { Popover, OverlayTrigger } from "react-bootstrap";
+import { Popover, OverlayTrigger, Button, Tooltip } from "react-bootstrap";
 
 export default class Track extends Component {
   constructor(props) {
     super(props);
-    const openPlane = (
-      <Popover>
-        <Popover.Title as="h3">title</Popover.Title>
-        <Popover.Content>
-          This is the content {this.props.message}
-        </Popover.Content>
-      </Popover>
-    );
     this.state = {
-      displayMessage: openPlane
+      displayMessage: (
+        <Popover>
+          <Popover.Title as="h3">title</Popover.Title>
+          <Popover.Content>
+            This is the content {this.props.message}
+          </Popover.Content>
+        </Popover>
+      ),
+      simpleMessage: <h3>Hello I am plane</h3>
     };
+    this.displayMessagePopup = this.displayMessagePopup.bind(this);
+  }
+
+  displayMessagePopup(props) {
+    return (
+      <Tooltip id="button-tooltip" {...props}>
+        info: {this.props.message}
+      </Tooltip>
+    );
   }
 
   render() {
     return (
       <div>
         <OverlayTrigger
-          trigger="click"
-          placement="left"
-          overlay={this.state.displayMessage}
+          placement="top"
+          delay={{ show: 250, hide: 400 }}
+          overlay={this.displayMessagePopup}
         >
           <Image src={require("../images/paper-plane-3.png")}></Image>
         </OverlayTrigger>
-
-        <div>
-          <h1>My info is {this.props.message}</h1>
-        </div>
       </div>
     );
   }
