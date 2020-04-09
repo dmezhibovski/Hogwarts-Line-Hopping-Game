7ec7858606dee3ab1be40b67ec8e6d14a98bb88e
unknown <calvin@stuntfactory.com>
2020-04-09 17:58:36 -0400

    Added basic algo struct

---
+++ src/components/algorithmStructure.js
@@ -0,0 +1,16 @@
+class basicAlgo {
+  constructor(startTrack, maxTrack) {
+    this.track = startTrack;
+    this.maxTrack = maxTrack;
+    this.planeInfoLog = []; //an array of all the planes you recieved
+  }
+
+  //info is track that you got hit on
+  receiveHit(info) {}
+
+  //info should be in array [track number, time]
+  receivePlane(info) {}
+
+  //info is track you teleported to
+  receiveMove(info) {}
+}
