b91cfb646a29d81dcc7362da02f42718f6257e18
unknown <calvin@stuntfactory.com>
2020-04-10 11:25:08 -0400

    Added multiple tracks

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,30 +1,55 @@
-import React, { Component } from 'react'
-import Image from 'react-bootstrap/Image'
+import React, { Component } from "react";
+import Image from "react-bootstrap/Image";
 // import Timer from './Timer'
 
-
-
 export default class Track extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+  }
+
+  renderMultTracks(numOfTracks) {
+    let trackLen = toString(100 / numOfTracks) + "%";
+    let tracks = [];
+    for (let i = 0; i < numOfTracks; i++) {
+      tracks.push(
+        <Image
+          src={require("./../images/tilted-tracks.png")}
+          className="track"
+          fluid
+          width="100%"
+          height={trackLen}
+        ></Image>
+      );
     }
+    return tracks;
+  }
 
-
-    render() {
-        return (
-            <div>
-                <div className='container-fluid justify-content-between'>
-                    <div className='col-3 position-relative'>
-                            <Image src={require('./../images/user.png')} className='bobbing' fluid />
-                    </div>
-                </div>
-                <Image src={require('./../images/tilted-tracks.png')} className='track' fluid />
-                {/* <div>
+  render() {
+    return (
+      <div>
+        <div className="container-fluid justify-content-between">
+          <div className="col-3 position-relative">
+            <Image
+              src={require("./../images/user.png")}
+              className="bobbing"
+              fluid
+            />
+          </div>
+        </div>
+        {this.renderMultTracks(9)}
+        {/*
+        <Image
+          src={require("./../images/tilted-tracks.png")}
+          className="track"
+          fluid
+        /> 
+        */}
+        {/* <div>
                     <h3 className='text-light font-weight-light'>
                         Track: {this.props.track}
                     </h3>
                 </div> */}
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
